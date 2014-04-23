1708da618e6aa05c4644605e74f3db7b65fc8908
rasingh <rishabh.alaapsingh@gmail.com>
2014-04-23 19:40:00 -0400

    added wrappers to use as interface functions

--- server/osc-client.py
+++ server/osc-client.py
@@ -1,15 +1,14 @@
 import OSC
 import time, random
 
-send_address = ('127.0.0.1', 7770)
+def osc_setup(ip_addr, port):
+    send_address = (ip_addr, port)
+    c = OSC.OSCClient()
+    c.connect( send_address ) # set the address for all following messages
 
-# OSC basic client
-c = OSC.OSCClient()
-c.connect( send_address ) # set the address for all following messages
+def osc_send_midi(midi_note):
+    msg = OSC.OSCMessage()
+    msg.setAddress("/midi") # set OSC address
+    msg.append(44) # int
+    c.send(msg) # send it!
 
-# single message
-msg = OSC.OSCMessage()
-msg.setAddress("/midi") # set OSC address
-msg.append(44) # int
-
-c.send(msg) # send it!
