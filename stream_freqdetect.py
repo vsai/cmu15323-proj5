3afc722a78d3d163ab29296464484bbb94196a7c
rasingh <rishabh.alaapsingh@gmail.com>
2014-04-27 21:06:49 -0400

    bugfixes

--- stream_freqdetect.py
+++ stream_freqdetect.py
@@ -8,23 +8,17 @@ import sys
 import time
 import struct
 import freq_bucketing as bt
-#import OSC
 from client import osc_client as oscc
 
 #settings for OSC
-#IPADDR = raw_input("Enter IP Address:")
-IPADDR = "128.237.191.102"
+IPADDR = raw_input("Enter IP Address:")
 PORT = 7770
 conn = oscc.osc_setup(IPADDR, PORT)
 CHANNEL_NAME = raw_input("Enter your midi channel name (1 or 2):")
 
-# Rishabh code
 CONF_SIZE = int(raw_input("Enter your confidence size:"))
 CONFIDENCE_ARR = [""] * CONF_SIZE
 CONF_IDX = 0
-# /Rishabh code
-
-
 
 # settings for pyaudio - open stream
 WIDTH = 2
@@ -43,13 +37,14 @@ def getMainFreq(indata, frame_count):
         x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
         # find the frequency and output it
         thefreq = (which+x1)*RATE/frame_count
-        #print "The freq is %f Hz." % (thefreq)
     else:
         thefreq = which*RATE/frame_count
-        #print "The freq is %f Hz." % (thefreq)
     return thefreq
 
 def callback(in_data, frame_count, time_info, status):
+
+    global CONF_IDX
+
     window = np.blackman(frame_count)
     # unpack the data and times by the hamming window
     indata = np.array(wave.struct.unpack('{n}h'.format(n=frame_count), in_data))*window
@@ -58,18 +53,13 @@ def callback(in_data, frame_count, time_info, status):
     print "Note is ", note
     midiNote = bt.getMusic(freq, "midi")
 
-    # Rishabh code
-    global CONF_IDX
+    
     CONFIDENCE_ARR[CONF_IDX % CONF_SIZE] = midiNote
     if (eltsEqual(CONFIDENCE_ARR)):
         oscc.osc_send_midi(conn, CHANNEL_NAME, midiNote)
-        print "sent message!!"
 
     CONF_IDX = CONF_IDX + 1
-    # /Rishabh code
 
-
-    #oscc.osc_send_midi(conn, CHANNEL_NAME, midiNote)
     return (in_data, pyaudio.paContinue)
 
 stream = p.open(format=p.get_format_from_width(WIDTH),
@@ -81,36 +71,29 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
                 stream_callback=callback)
 
 
-# Rishabh code
-
+# Checks if all elements in the array are the same
 def eltsEqual(array):
     length = len(array)
     if length <= 1:
-        print "only one elt in array!!"
         return True
     first = array[0]
     for i in range(1,length):
         if array[i] != first:
             print "elts do not match!", array
             return False
-
-    print "all elts match!"
     return True
 
-# /Rishabh code
 
-
-
-#start the stream
+# Start the stream
 stream.start_stream()
 
-#wait for stream to finish
+# Wait for stream to finish
 while stream.is_active():
     time.sleep(0.1)
 
-#stop stream
+# Stop stream
 stream.stop_stream()
 stream.close()
 
-#close PyAudio
+# Close PyAudio
 p.terminate()
